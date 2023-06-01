c450395377120de594b45b0b23339bcb8e542f1e
200813567 <200813567@lbschools.net>
2023-06-01 13:44:40 -0700

    MainActivity has tv, ll, et, and btn.

--- app/src/main/java/com/example/madlib/MainActivity.java
+++ app/src/main/java/com/example/madlib/MainActivity.java
@@ -3,8 +3,40 @@ package com.example.madlib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitle;
+
+    LinearLayout llHintedWords;
+
+    EditText etAdjective;
+
+    EditText etNoun;
+
+    EditText etTime;
+
+    EditText etNumber;
+
+    EditText etDate;
+
+    EditText etAnimal;
+
+    EditText etNoun2;
+
+    EditText etTime2;
+
+    EditText etEmotion;
+
+    EditText etAdjective2;
+
+    EditText etFood;
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

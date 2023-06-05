557cf39eaff383bc37f0efbbf8b73a2951009a2d
200813567 <200813567@lbschools.net>
2023-06-05 13:39:39 -0700

    First activity in MainActivity.java exampled followed

--- app/src/main/java/com/example/madlib/MainActivity.java
+++ app/src/main/java/com/example/madlib/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.madlib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -36,14 +39,54 @@ public class MainActivity extends AppCompatActivity {
 
     EditText etFood;
 
-    Button btnStart;
+    Button btnContinue;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //linking to layout
+        tvTitle = findViewById(R.id.size);
+        llHintedWords = findViewById(R.id.ll_hintedwords);
+        etAdjective = findViewById(R.id.et_adjective);
+        etNoun2 = findViewById(R.id.et_noun);
+        etNumber = findViewById(R.id.et_number);
+        etDate = findViewById(R.id.et_date);
+        etAnimal = findViewById(R.id.et_animal);
+        etTime2 = findViewById(R.id.et_time2);
+        etEmotion = findViewById(R.id.et_emotion);
+        etAdjective2 = findViewById(R.id.et_adjective2);
+        etFood = findViewById(R.id.et_food);
+        btnContinue = findViewById(R.id.btn_continue);
 
+        //intent to next activity
+        Intent intentDisplay = new Intent(MainActivity.this, DisplayActivity.class);
+
+        //generate madlib
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //generate story
+                String story = "";
+                story += "\n..." + etAdjective.getText();
+                story += "\n..." + etNoun.getText();
+                story += "\n..." + etTime.getText();
+                story += "\n..." + etNumber.getText();
+                story += "\n..." + etDate.getText();
+                story += "\n..." + etAnimal.getText();
+                story += "\n..." + etNoun2.getText();
+                story += "\n..." + etTime2.getText();
+                story += "\n..." + etEmotion.getText();
+                story += "\n..." + etAdjective2.getText();
+                story += "\n..." + etFood.getText();
+
+                //pass to next activity
+                intentDisplay.putExtra("Lost",story);
+                startActivity(intentDisplay);
+            }
+        });
 
     }
 }
